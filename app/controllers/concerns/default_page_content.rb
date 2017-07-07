f2628546b273d57069c589c671edeaa9ff018f38
fjasonvasquez <fjvasquez@aol.com>
2017-07-07 01:06:27 -0700

    controller manipulation for views

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,12 @@
+module DefaultPageContent
+	extend ActiveSupport::Concern
+
+	included do 
+		before_filter :set_page_defaults
+	end
+
+  def set_title
+  	@page_title = "Jason Portfolio | My Portfolio Website"
+  	@seo_keywords = "Jason Vasquez Portfolio"
+  end
+end
