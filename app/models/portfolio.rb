5122bc28fa49a103216189853e9ea22dbc60ead7
fjasonvasquez <fjvasquez@aol.com>
2017-09-07 23:10:53 -1000

    Updatex file upload forms and refactored placeholder functionality into view

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,7 +3,7 @@ class Portfolio < ApplicationRecord
 	accepts_nested_attributes_for :technologies, 
 																reject_if: lambda { |attrs| attrs['name'].blank? }
 
-	validates_presence_of :title, :body, :main_image, :thumb_image
+	validates_presence_of :title, :body
 
 	mount_uploader :thumb_image, PortfolioUploader
 	mount_uploader :main_image, PortfolioUploader
