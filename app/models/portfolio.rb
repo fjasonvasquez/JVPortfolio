b515a0395c8a2947d04c3514d6511a0e1e7b505d
fjasonvasquez <fjvasquez@aol.com>
2017-09-12 22:48:26 -1000

    modified js applications, and added cocoon for portfolio management

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,7 @@
 class Portfolio < ApplicationRecord
 	has_many :technologies
-	accepts_nested_attributes_for :technologies, 
+	accepts_nested_attributes_for :technologies,
+																allow_destroy: true, 
 																reject_if: lambda { |attrs| attrs['name'].blank? }
 
 	validates_presence_of :title, :body
@@ -18,9 +19,9 @@ class Portfolio < ApplicationRecord
 
 	scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 
-	after_intiialize :set_defaults
+	after_initialize :set_defaults
 
-	def set_defautlts
+	def set_defaults
 		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
 		self.main_image ||= Placeholder.image_generator(height: '350', width: '4200')
 	end
