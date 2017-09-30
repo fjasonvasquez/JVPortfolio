4ad5daba0a7c2155ad1e9bd65e38aeb28ef65221
fjasonvasquez <fjvasquez@aol.com>
2017-09-29 21:46:47 -1000

    Completed comment feature with action cable

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,5 +1,5 @@
 module BlogsHelper
 	def gravatar_helper user
-		image_tag "https://www.gravatar.com/avatar,/#{gravatar id}",width: 40" 
+		image_tag "https://www.gravatar.com/avatar,/#{gravatar id}",width: 60
 	end	
 end
